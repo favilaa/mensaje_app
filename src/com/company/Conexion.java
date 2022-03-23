05715c07e60c13ac32e45afa9c6c63f95a839604
Fernando Shaid Avila Avila <avila.avila.21@gmail.com>
2022-03-22 19:41:18 -0600

    first commit

---
+++ src/com/company/Conexion.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_connection(){
+        Connection conection = null;
+        try{
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensaje_app","root","");
+            if(conection != null){
+                System.out.println("Conexión exitosa");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return conection;
+    }
+}
---
+++ src/com/company/Inicio.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.Connection;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+
+        Conexion conexion = new Conexion();
+
+        try(Connection cnx = conexion.get_connection()){
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
